268f3ead8827cdbcbfbdfeb8917c72ed86f030ad
yunmin.nam <yunmin.nam@qcells.com>
2025-04-14 18:34:15 +0900

    fix files

--- vite.config.js
+++ vite.config.js
@@ -22,5 +22,8 @@ export default defineConfig({
       formats: ["iife"],
     },
   },
-  base: "/",
+  base: "/create-widget/",
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
 });
